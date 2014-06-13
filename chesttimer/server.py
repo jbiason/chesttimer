09ec18d71b2b710c902ff182d3f31e1c42242729
Julio Biason <julioandre@cwi.com.br>
2014-06-13 09:05:34 -0300

    finally, start the server

---
+++ chesttimer/server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""Main server/controller."""
+
+# ChestTimer, an agenda creator for GW2 chests.
+# Copyright (C) 2014 Julio Biason
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.ArithmeticError#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from flask import Flask
+
+from .settings import Settings
+
+# ----------------------------------------------------------------------
+# Start the app
+# ----------------------------------------------------------------------
+app = Flask(__name__)
+app.config.from_object(Settings)
+app.config.from_envvar('CHESTTIMER_CONFIG', True)
