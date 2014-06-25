f9bc47238f5cee292ae57d34a6a5abbc653a998f
Julio Biason <julioandre@cwi.com.br>
2014-06-25 10:55:17 -0300

    added dummy data to start coding the angular part

--- chesttimer/static/controllers.js
+++ chesttimer/static/controllers.js
@@ -1,3 +1,50 @@
 function CharacterController($scope) {
-  $scope.groups = [];
+  $scope.groups = [
+            {
+                "characters": [
+                    {
+                        "disciplines": null,
+                        "level": 25,
+                        "name": "Sgt Buzzkill",
+                        "order": null,
+                        "profession": "warrior",
+                        "race": "human",
+                        "sex": "female",
+                        "slug": "sgt_buzzkill"
+                    }
+                ],
+                "group": 25
+            },
+            {
+                "characters": [
+                    {
+                        "disciplines": {
+                            "armorsmith": 500,
+                            "weaponsmith": 415
+                        },
+                        "level": 80,
+                        "name": "Thorianar",
+                        "order": "durmand_priori",
+                        "profession": "guardian",
+                        "race": "charr",
+                        "sex": "male",
+                        "slug": "thorianar"
+                    },
+                    {
+                        "disciplines": {
+                            "huntsman": 400,
+                            "leatherworker": 500
+                        },
+                        "level": 80,
+                        "name": "Commander Buzzkill",
+                        "order": "durmand_priori",
+                        "profession": "engineer",
+                        "race": "charr",
+                        "sex": "male",
+                        "slug": "commander_buzzkill"
+                    }
+                ],
+                "group": 80
+            }
+    ];
 }
