7400cff308caab0f83cedad6b7efdab49f3ca987
Julio Biason <julioandre@cwi.com.br>
2014-06-26 11:29:32 -0300

    tried to make everything a factory, but it seems that is not the way to go

--- chesttimer/static/controllers.js
+++ chesttimer/static/controllers.js
@@ -5,51 +5,51 @@ chesttimerModule.factory('Characters', function () {
   chars.query = function() {
     // ajax goes here
     return [{"characters": [{
-                        "disciplines": null,
-                        "level": 25,
-                        "name": "Sgt Buzzkill",
-                        "order": null,
-                        "profession": "warrior",
-                        "race": "human",
-                        "sex": "female",
-                        "slug": "sgt_buzzkill"
-                    }
-                ],
-                "group": 25
-            },
-            {
-                "characters": [
-                    {
-                        "disciplines": {
-                            "armorsmith": 500,
-                            "weaponsmith": 415
-                        },
-                        "level": 80,
-                        "name": "Thorianar",
-                        "order": "durmand_priori",
-                        "profession": "guardian",
-                        "race": "charr",
-                        "sex": "male",
-                        "slug": "thorianar"
-                    },
-                    {
-                        "disciplines": {
-                            "huntsman": 400,
-                            "leatherworker": 500
-                        },
-                        "level": 80,
-                        "name": "Commander Buzzkill",
-                        "order": "durmand_priori",
-                        "profession": "engineer",
-                        "race": "charr",
-                        "sex": "male",
-                        "slug": "commander_buzzkill"
-                    }
-                ],
-                "group": 80
-            }
-    ];
-  };
+      "disciplines": null,
+      "level": 25,
+      "name": "Sgt Buzzkill",
+      "order": null,
+      "profession": "warrior",
+      "race": "human",
+      "sex": "female",
+      "slug": "sgt_buzzkill"
+    }
+    ],
+    "group": 25
+  },
+  {
+    "characters": [
+    {
+      "disciplines": {
+        "armorsmith": 500,
+        "weaponsmith": 415
+      },
+      "level": 80,
+      "name": "Thorianar",
+      "order": "durmand_priori",
+      "profession": "guardian",
+      "race": "charr",
+      "sex": "male",
+      "slug": "thorianar"
+    },
+    {
+      "disciplines": {
+        "huntsman": 400,
+        "leatherworker": 500
+      },
+      "level": 80,
+      "name": "Commander Buzzkill",
+      "order": "durmand_priori",
+      "profession": "engineer",
+      "race": "charr",
+      "sex": "male",
+      "slug": "commander_buzzkill"
+    }
+    ],
+    "group": 80
+  }
+  ];
+};
   return chars;
 });
 
@@ -63,7 +63,65 @@ chesttimerModule.factory('Sexes', function () {
   return sexes;
 });
 
-chesttimerModule.controller('CharacterController', function (Sexes, Characters, $scope) {
+chesttimerModule.factory('Race', function () {
+  var races = {};
+  races.query = function () {
+    return {'norn': 'Norn',
+    'sylvari': 'Sylvari',
+    'human': 'Human',
+    'charr': 'Charr',
+    'asura': 'Asura'};
+  };
+  return races;
+});
+
+chesttimerModule.factory('Order', function () {
+  var orders = {};
+  orders.query = function () {
+    return {'order_of_whispers': 'Order of Whispers',
+      'vigil': 'Vigil',
+      'durmand_priori': 'Durmand Priori'};
+  };
+  return orders;
+});
+
+chesttimerModule.factory('Disciplines', function () {
+  var disciplines = {};
+  disciplines.query = function () {
+    return {'armorsmith': 'Armorsmith',
+      'weaponsmith': 'Weaponsmith',
+      'artificer': 'Artificer',
+      'chef': 'Chef',
+      'tailor': 'Tailor',
+      'huntsman': 'Huntsman',
+      'jeweler': 'Jeweler',
+      'leatherworker': 'Leatherworker'
+
+    }
+  };
+  return disciplines;
+});
+
+chesttimerModule.factory('Professions', function () {
+  var professions = {};
+  professions.query = function () {
+    return {'guardian': 'Guardian',
+    'warrior': 'Warrior',
+    'necromancer': 'Necromancer',
+    'mesmer': 'Mesmer',
+    'elementalist': 'Elementalist',
+    'ranger': 'Ranger',
+    'thief': 'Thief',
+    'engineer': 'Engineer'}
+  };
+  return professions;
+})
+
+chesttimerModule.controller('CharacterController', function (Professions, Disciplines, Orders, Races, Sexes, Characters, $scope) {
   $scope.sexes = Sexes.query();
+  $scope.races = Races.query();
+  $scope.orders = Orders.query();
+  $scope.disciplines = Disciplines.query();
+  $scope.professions = Professions.query();
   $scope.groups = Characters.query();
 });
